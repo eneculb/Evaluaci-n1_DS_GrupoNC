cb5091a18a11196025a9ef101ef892adadd4b6a9
tsengian36-rgb <tsengian36@gmail.com>
2026-05-22 23:19:08 -0400

    Initialize TRABAJ.py with necessary imports

---
+++ TRABAJ.py
@@ -0,0 +1,15 @@
+# INTEGRANTES:
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
+# DATOS
